16bf2cc03e26ed412bc55b7c9c2f7bad0cbe32b9
Scott Forman <sjforman@gmail.com>
2013-12-12 16:08:02 -0500

    Functioning text-only version.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 var timeoutHandle;
-var encourageInterval = 10
+
 var mantrasArray = [];
-var mantras = document.getElementsByClassName('mantra');
+var mantras = $('.mantra');
+var mantrasdiv = $('#mantras');
+var mantrasList = $('#mantraslist');
+
+var INTERVAL = 120;
+var DELAY = 10;
 
 for (var i = 0; i < mantras.length; i++) {
   mantrasArray.push(mantras[i].innerHTML);
 }
-
-console.log(mantrasArray);
 
 function timer(timerid, timeInSeconds, callback) {
 
@@ -19,11 +22,11 @@ function timer(timerid, timeInSeconds, callback) {
   if (t > 0) {
     t--;
     countdown.innerHTML = formatTime(t);   
-    timeoutHandle = window.setTimeout(function() {timer(timerid, t, callback)},100);
+    timeoutHandle = window.setTimeout(function() {timer(timerid, t, callback)},DELAY);
   }
   else if (t == 0) {
     callback();
-    timer(timerid, encourageInterval, callback);
+    timer(timerid, INTERVAL, callback);
   }
 
   function formatTime() {
@@ -32,15 +35,48 @@ function timer(timerid, timeInSeconds, callback) {
   }
 }
 
-function switchMantra() {
-  var currentMantra = document.getElementById('mantra');
+function nextMantra() {
+  var currentMantra = document.getElementById('currentmantra');
   var i = mantrasArray.indexOf(currentMantra.innerHTML);
-  if (i == mantras.length - 1) {
-    currentMantra.innerHTML = mantrasArray[0];
+  var numMantras = mantrasArray.length;
+  if (numMantras == 0) {
+    currentMantra.innerHTML = "There aren't any mantras";
     }
-  else {
+  else if (i < numMantras - 1) {
     currentMantra.innerHTML = mantrasArray[i + 1];
+    }
+  else if (i == numMantras - 1) {
+    currentMantra.innerHTML = mantrasArray[0];
   }
 }
 
-timer('countdown', encourageInterval, function() {switchMantra()});
+function submit() {
+  var field = $('#custommantra');
+  var custom = field.val();
+  var numMantras = mantrasList.length;
+  var newMantraIndex = numMantras + 1;
+  if (custom) {
+    mantrasArray.push(custom);
+    mantrasList.append( "<li id='mantra" + newMantraIndex + "' data-icon='delete'><a class='mantra' href='#play'>" + custom + "</a> <a class='deleteButton' href='#delete'>Delete</a></li>");
+    $('#mantraslist').listview('refresh');
+    field.val('');
+  }
+  else {
+    alert('Please enter a mantra');
+  }
+  field.focus();
+}
+
+function deleteMantra(mantraId) {
+  var targetmantraContainer = $('#' + mantraId);
+  var targetmantraText = $('#' + mantraId + " .mantra")[0].innerHTML;
+  var targetMantraIndex = mantrasArray.indexOf(targetmantraText);
+  mantrasArray.splice(targetMantraIndex, targetMantraIndex + 1);
+  targetmantraContainer.remove();
+}
+
+$("#mantraslist").on("click", ".deleteButton", (function() {
+  deleteMantra( $(this).parent().attr('id'));
+  }));
+
+timer('countdown', INTERVAL, function() {nextMantra()});
