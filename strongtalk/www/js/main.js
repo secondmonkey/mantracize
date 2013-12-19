5dd24809d0002920a3d6202dd0ab86d13dd08895
Scott Forman <sjforman@gmail.com>
2013-12-19 17:02:53 -0500

    Now records audio

--- strongtalk/www/js/main.js
+++ strongtalk/www/js/main.js
@@ -79,4 +79,105 @@ $("#mantraslist").on("click", ".deleteButton", (function() {
   deleteMantra( $(this).parent().attr('id'));
   }));
 
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, function fail(){});
+  console.log("device is ready");
+}
+
+
+// Setup audio recording
+//
+var gotFS = function (fileSystem) {
+    fileSystem.root.getFile("blank.wav",
+        { create: true, exclusive: false }, //create if it does not exist
+        function success(entry) {
+            var src = entry.toURI();
+            console.log(src); //logs blank.wav's path starting with file://
+        },
+        function fail() {}
+    );
+};
+
+function recordAudio() {
+    var src = "blank.wav";
+    var mediaRec = new Media(src, onSuccess, onError);
+
+    // Record audio
+    mediaRec.startRecord();
+
+    // Stop recording after 10 sec
+    var recTime = 0;
+    var recInterval = setInterval(function() {
+        recTime = recTime + 1;
+        if (recTime >= 10) {
+            clearInterval(recInterval);
+            mediaRec.stopRecord();
+        }
+    }, 1000);
+}
+
+//function onSuccess() {
+    //console.log("recordAudio():Audio Success");
+//}
+
+ //onError Callback 
+//function onError(error) {
+    //alert('code: '    + error.code    + '\n' + 
+          //'message: ' + error.message + '\n');
+//}
+
+$('#record-button').
+bind('tap', function(){
+    recordAudio();
+})
+
+// Audio player
+//
+var my_media = null;
+
+// Play audio
+//
+function playAudio(src) {
+  if (my_media == null) {
+    // Create Media object from src
+    my_media = new Media(src, onSuccess, onError);
+  } // else play current audio
+  // Play audio
+  console.log(src);
+  my_media.play();
+}
+
+// Pause audio
+//
+function pauseAudio() {
+  if (my_media) {
+    my_media.pause();
+  }
+}
+
+// Stop audio
+//
+function stopAudio() {
+  if (my_media) {
+    my_media.stop();
+  }
+}
+
+// onSuccess Callback
+//
+function onSuccess() {
+  console.log("playAudio():Audio Success");
+}
+
+// onError Callback
+//
+function onError(error) {
+  alert('code: '    + error.code    + '\n' +
+      'message: ' + error.message + '\n');
+}
+
 timer('countdown', INTERVAL, function() {nextMantra()});
